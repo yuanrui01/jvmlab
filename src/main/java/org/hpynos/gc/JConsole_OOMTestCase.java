4756dfc567588a65fb0071d6ee3493fc1aa64ddd
yuanrui <xdyrfree@gmail.com>
2024-11-25 22:57:15 +0800

    补充jvm实验代码 - oom

---
+++ src/main/java/org/hpynos/gc/JConsole_OOMTestCase.java
@@ -0,0 +1,31 @@
+package org.hpynos.gc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JConsole_OOMTestCase {
+
+
+    /**
+     * 内存占位符对象，一个OOMObject大约占64KB
+     */
+    static class OOMObject {
+        public byte[] placeholder = new byte[64 * 1024];
+    }
+
+    public static void fillHeap(int num) throws InterruptedException {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        for (int i = 0; i < num; i++) {
+            // 稍作延时，令监视曲线的变化更加明显
+            Thread.sleep(50);
+            list.add(new OOMObject());
+        }
+        //System.gc();
+    }
+
+    // -Xms100m -Xmx100m -XX:+UseSerialGC
+    public static void main(String[] args) throws Exception {
+        fillHeap(1000);
+        System.gc();
+    }
+}
